73511917183cf3bb82912dbad439f7daa36403da
anuragraiofficial321 <anuragraiofficial321@gmail.com>
2023-08-26 15:27:25 +0530

    Download Links are working

--- mainapp.py
+++ mainapp.py
@@ -1,6 +1,14 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    send_from_directory,
+)
 from pytube import Playlist, YouTube
+import re  # Import the re module for regular expressions
 
 app = Flask(__name__)
 
@@ -14,31 +22,55 @@ def index():
 def download():
     playlist_url = request.form["playlist_url"]
     playlist = Playlist(playlist_url)
-    path = os.path.join(os.path.abspath("."), playlist.title)
+    download_path = os.path.join(os.path.abspath("."), "downloads")
 
-    if not os.path.isdir(path):
-        os.mkdir(path)
+    if not os.path.exists(download_path):
+        os.mkdir(download_path)
 
+    video_links = []  # Store video download links
+    c = 0
     for url in playlist:
         try:
+            c = c + 1
+            if c == 3:
+                break
+
             video = YouTube(url)
             print(f"Downloading {video.title}")
+            title = video.title.strip()
+            print(title)
+
+            # Sanitize the video title for use in URLs
+            sanitized_title = re.sub(r"[^a-zA-Z0-9]", "_", title)
 
             # Check if the file already exists before downloading again
-            video_path = os.path.join(path, f"{video.title}.mp4")
+            video_path = os.path.join(download_path, f"{sanitized_title}.mp4")
 
             if os.path.isfile(video_path):
                 print("Video already exists. Skipping...")
-                continue
+            else:
+                stream = video.streams.get_highest_resolution()
+                stream.download(
+                    output_path=download_path, filename=f"{sanitized_title}.mp4"
+                )
 
-            stream = video.streams.get_highest_resolution()
-            stream.download(output_path=path)
-            print("Download completed.")
+            # Generate download link for the user
+            video_links.append((title, f"/downloads/{sanitized_title}.mp4"))
 
         except Exception as e:
             print(f"An error occurred while downloading the video: {e}")
 
-    return redirect(url_for("index"))
+    return render_template("download.html", video_links=video_links)
+
+
+@app.route("/downloads/<filename>")
+def serve_file(filename):
+    # Ensure the requested file exists
+    file_path = os.path.join(os.path.abspath("."), "downloads", filename)
+    if os.path.isfile(file_path):
+        return send_from_directory("downloads", filename)
+    else:
+        return "File not found", 404
 
 
 if __name__ == "__main__":
